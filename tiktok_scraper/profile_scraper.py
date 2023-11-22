e366211c946b628d8071e92622f44f8e8c325dfe
Owen Slagle <owenslagle@blueyoshi.com>
2023-11-22 06:25:06 -0500

    finalized scraping tiktok video data

--- tiktok_scraper/profile_scraper.py
+++ tiktok_scraper/profile_scraper.py
@@ -9,6 +9,7 @@ CLASS = {
     'video_image' : 'tiktok-x6f6za-DivContainer-StyledDivContainerV2 eq741c50',
     'video_description' : 'tiktok-vi46v1-DivDesContainer eih2qak4',
     'video_link' : 'tiktok-1wrhn5c-AMetaCaptionLine eih2qak0',
+    'views' : 'video-count tiktok-dirst9-StrongVideoCount e148ts222'
 }
 
 
@@ -23,13 +24,15 @@ def get_num_videos(html: str) -> int:
 def load_all_videos(driver: webdriver.Chrome,
                     initial_wait: int = 5,
                     scroll_wait: int = 3):
+    print("Loading all videos...")
+    
     time.sleep(initial_wait)
     
     num_scrolls = 0
     num_videos_pre_scroll = get_num_videos(driver.page_source)
     num_videos_post_scroll = 0
     
-    print(f"Found {num_videos_pre_scroll} videos before scrolling")
+    print(f"Found {num_videos_pre_scroll} videos before scrolling.")
     
     while num_videos_pre_scroll != num_videos_post_scroll:
         # Scroll to the bottom of the page to lead all videos
@@ -43,13 +46,13 @@ def load_all_videos(driver: webdriver.Chrome,
         num_videos_pre_scroll = num_videos_post_scroll
         num_videos_post_scroll = get_num_videos(driver.page_source)
         
-        print(f"Found {num_videos_post_scroll} videos after {num_scrolls} scrolls")
+        print(f"Found {num_videos_post_scroll} videos after {num_scrolls} scrolls.")
     
-    print(f"Loaded all {num_videos_post_scroll} videos after {num_scrolls} scrolls")
+    print(f"Found {num_videos_post_scroll} videos total.\n")
 
 
 # Scrapes a TikTok profile for all video links
-def get_all_video_links(user_tag: str = '@aimusicpainter') -> list:
+def get_all_video_links_with_views(user_tag: str = '@aimusicpainter') -> list:
     # Set up the browser
     driver = webdriver.Chrome()
     driver.get('https://www.tiktok.com/' + user_tag)
@@ -62,20 +65,27 @@ def get_all_video_links(user_tag: str = '@aimusicpainter') -> list:
     soup = BeautifulSoup(html, 'html.parser')
     
     # Get each tag with a video link
+    print("Finding video link tags...")
     video_link_tags = soup.find_all('a', class_=CLASS['video_link'])
-    print(f"Found {len(video_link_tags)} video link tags")
+    print(f"Found {len(video_link_tags)} video link tags.\n")
     
-    # Get each video link
-    video_links = []
-    for tag in video_link_tags:
-        video_links.append(tag['href'])
+    # Get the view count for each video
+    print("Finding view counts...")
+    video_view_tags = soup.find_all('strong', class_=CLASS['views'])
+    print(f"Found {len(video_view_tags)} view count tags.\n")
+    
+    
+    # Get each video link and view count
+    videos = []
+    for link_tag, view_tag in zip(video_link_tags, video_view_tags):
+        videos.append([link_tag['href'], int(view_tag.text)])
     
     # Close the browser
     driver.close()
     
-    return video_links
+    return videos
 
 
 if __name__ == '__main__':
-    video_links = get_all_video_links()
-    print(video_links)
+    videos = get_all_video_links_with_views()
+    print(videos)
