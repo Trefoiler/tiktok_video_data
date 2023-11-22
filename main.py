650a2425c2a81b3b4c525be63c68b9dafb337f2c
Owen Slagle <owenslagle@blueyoshi.com>
2023-11-22 06:58:23 -0500

    main.py shows time elapsed

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import time
 from tiktok_scraper.video_info_finalizer import get_info_from_all_videos
 from utils.save_video_data import save_data
 
+
 USER_TAG = '@aimusicpainter'
+
+
+start_time = time.time()
 
 all_data = get_info_from_all_videos(USER_TAG)
 save_data(all_data, USER_TAG)
+
+print(f"Finished in {time.time() - start_time} seconds")
